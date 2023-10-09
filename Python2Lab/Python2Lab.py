134ae9f7e1f8354fd81905b76243c66ecc831496
Qwerty000101 <andreyiv20005@gmail.com>
2023-10-09 09:11:40 +0300

    Change6

--- Python2Lab/Python2Lab.py
+++ Python2Lab/Python2Lab.py
@@ -14,3 +14,11 @@ turtle.right(180)
 turtle.forward(0)
 turtle.pensize(25)
 turtle.goto(330,80)
+turtle.down()
+while i<6:
+    turtle.color("red")
+    turtle.pensize(25)
+    turtle.hideturtle()
+    turtle.forward(650)
+    turtle.left(144)
+    i+=1 
